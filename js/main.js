43249f80a2a1680ebb6007fc222c865928503ecf
Matt Daniel Brown <developer@mattdanielbrown.com>
2022-10-19 00:40:22 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+"use strict";
+
+
+function main() {
+	const mobileNavControl = document.getElementById('mobile-nav-control');
+	// const navigationList = document.querySelector('nav > ul');
+	const navigationBar = document.querySelector('nav');
+	const mobileNavControlLabel = document.querySelector('#mobile-nav-control ~ label');
+	
+	document.addEventListener("touchstart", function () {
+	}, true);
+	
+	mobileNavControl.addEventListener('change', function () {
+		navigationBar.classList.toggle('displayed');
+		if (this.checked === true) {
+			mobileNavControlLabel.innerHTML = 'Close';
+		} else {
+			mobileNavControlLabel.innerHTML = 'Menu';
+		}
+	});
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+	console.log('DOM fully loaded and parsed');
+	main();
+});
